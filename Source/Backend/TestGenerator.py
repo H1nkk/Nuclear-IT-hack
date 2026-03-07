99a122c8d385fb7122ccd5e953b91da9564a5458
Maxim <scourgetp@gmail.com>
2026-03-07 19:02:57 +0300

    [DEV] Dict -> List

--- Source/Backend/TestGenerator.py
+++ Source/Backend/TestGenerator.py
@@ -6,7 +6,7 @@ all_addr = list(range(0x0000, 0x0010))
 print(f"Amount = {len(all_addr)}")
 
 def test_all_registers(test_addr : list) -> list:
-    results = {}
+    results = []
     # 0 - OK
     # 1 - ack = false (reading)
     # 2 - ack = false (writing)
@@ -23,24 +23,24 @@ def test_all_registers(test_addr : list) -> list:
         try:
             writing = reg_access(addr, test_input, 'write')
             if not writing['ack']: # Err 1: ack = false in write
-                results[addr] = 1
+                results.append(1)
                 failed += 1
                 continue
             
             reading = reg_access(addr, 0, 'read')
             if not reading['ack']: # Err 2: ack = false in read
-                results[addr] = 2
+                results.append(2)
                 failed += 1
                 continue
 
             if (test_input != reading['reg_value']): # Err 3: Write != Read
-                results[addr] = 3
+                results.append(3)
                 failed += 1
             else:
-                results[addr] = 0
+                results.append(0)
                 passed += 1
         except Exception as e: # Err 4: Just died
-            results[addr] = 4
+            results.append(4)
             failed += 1
 
     # Stats  
@@ -48,29 +48,31 @@ def test_all_registers(test_addr : list) -> list:
     print(f"Passed: {passed}")
     print(f"Failed: {failed}")
 
-    for key, value in results.items():
+    number = 0
+    for value in results:
         if not value == 0:
             if value == 1:
-                print(f"Number: {key} Error: ack = false while writing")
+                print(f"Number: {number} Error: ack = false while writing")
             if value == 2:
-                print(f"Number: {key} Error: ack = false while reading")
+                print(f"Number: {number} Error: ack = false while reading")
             if value == 3:
-                print(f"Number: {key} Error: Data read != data write")
+                print(f"Number: {number} Error: Data read != data write")
             if value == 4:
-                print(f"Number: {key} Error: Exception is thrown")
-   
+                print(f"Number: {number} Error: Exception is thrown")
+        number += 1
+        
     return results
 
 def fourth_register_test(test_addr : list):
     for addr in test_addr:
-        if addr == 0x0004:
+        if addr < 0x0000:
             continue
         test_input = random.randint(0, 0xFFFF)
-        writing = reg_access(addr, test_input, 'write')            
+        writing = reg_access(addr, test_input, 'write')        
         reading = reg_access(0x0004, 0, 'read')
         if not reading['ack']:
             print(f"After addressing {addr}, fourth register is broken")
 
 
-# test_all_registers(all_addr)  
-fourth_register_test(all_addr)
+test_all_registers(all_addr)  
+# fourth_register_test(all_addr)
