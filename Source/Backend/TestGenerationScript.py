38a98e64fc60bb2e75367c9186b3fe9a4397c643
K <Akarfire@gmail.com>
2026-03-07 22:19:06 +0300

    [TEST] Generated Tests update

--- Source/Backend/TestGenerationScript.py
+++ Source/Backend/TestGenerationScript.py
@@ -2,12 +2,14 @@ def generate_tests(test_addr: list):
     with open("./Source/Tests/GeneratedTests.py", "w") as f:
 
         f.write(
-            "from Source.Backend.riscv_reg_block import reg_access\nimport random\n\n\nimport pytest\n\n\n"
+            "from Source.Backend.riscv_reg_block import reg_access\nimport random\n\nimport pytest\n\n"
         )
+
+        f.write("# Write Read All Value \n\n")
 
         for addr in test_addr:
             f.write(f"\
-def test_register_{addr}(): \n\
+def test_write_read_all_values_register_{addr}(): \n\
     for test_input in range(0x0000, 0xFFFF): \n\
         writing = reg_access({addr}, test_input, 'write') \n\
         if not writing['ack']: \n\
@@ -19,8 +21,27 @@ def test_register_{addr}(): \n\
 \n\
         if (test_input != reading['reg_value']): \n\
             assert False \n\
-\n\
     assert True\n\n\n\
+")
+        
+        f.write("# Overflow Test \n\n")
+        
+        for addr in test_addr:
+            f.write(f"\
+def test_overflow_register_{addr}(): \n\
+    test_input = 0xFFFFFF \n\
+    expected_output = 0xFFFF \n\
+    writing = reg_access({addr}, test_input, 'write') \n\
+    if not writing['ack']: \n\
+        assert False \n\
+\n\
+    reading = reg_access({addr}, 0, 'read') \n\
+    if not reading['ack']: \n\
+        assert False \n\
+\n\
+    if (expected_output != reading['reg_value']): \n\
+        assert False \n\
+    assert True \n\n\n\
 ")
 
 
